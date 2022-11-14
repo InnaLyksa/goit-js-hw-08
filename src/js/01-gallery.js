f9151c54192769da45468386bc648a22e87bd4f2
InnaLyksa <in.lyksa@gmail.com>
2022-11-14 16:13:16 +0200

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(item => {
+      return `
+  <a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
